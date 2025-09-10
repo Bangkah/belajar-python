759966343f548356e7cbb0a62133919d73243b94
MUHAMMAD DHIYAUL ATHA <mdhyaulatha@gmail.com>
2025-09-10 15:32:06 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# varibel data
+nama = "Atha"             # str
+umur = 18                 # int
+tinggi = 165.0            # float
+is_mahasiswa = True       # bool
+hobi = ["membaca", "olahraga"] # list
+alamat = "Lhoksemawe" # str
+tempat_lahir = "Meulaboh"  # str
+tanggal_lahir = "20-02-2007" # str
+
+# hasil
+
+print("Contoh Variabel dan Tipe Data:")
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi:", tinggi)
+print("Mahasiswa:", is_mahasiswa)
+print("Hobi:", hobi)
+print("Alamat:", alamat)
+print("Tempat Lahir:", tempat_lahir)
+print("Tanggal Lahir:", tanggal_lahir)
